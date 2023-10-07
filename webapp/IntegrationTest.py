d501115a69c15cefaba3a2d22d31306dacd0ea64
Shivani Shah <shivanishahwork2@gmail.com>
2023-10-06 23:15:21 -0700

    Adding flask web application with Github Workflows

---
+++ webapp/IntegrationTest.py
@@ -0,0 +1,14 @@
+import unittest
+from webapp import webapp
+import os
+
+class TestHealthz(unittest.TestCase):
+
+    def test_health_check():
+        app = webapp.test_client()
+        response = app.get('/healthz')
+        assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
+
