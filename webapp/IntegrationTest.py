2891a076272954625c1c463e6c271311aecbad74
Shivani Shah <113885451+ShivaniShah06@users.noreply.github.com>
2023-10-07 14:34:45 -0700

    Trying to fix IntegrationTest.py

--- webapp/IntegrationTest.py
+++ webapp/IntegrationTest.py
@@ -4,10 +4,10 @@ import os
 
 class TestHealthz(unittest.TestCase):
 
-    def test_health_check():
+    def test_health_check(self):
         app = webapp.test_client()
         response = app.get('/healthz')
-        assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
