7e914b9ea62e42fefd1a1f6860f0627d6afa7196
Shivani Shah <shivanishahwork2@gmail.com>
2023-11-08 23:52:26 -0800

    Downloading CloudWatch agent through shell script

--- webapp/install.sh
+++ webapp/install.sh
@@ -70,3 +70,8 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "Packages installation completed successfully."
+
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/debian/amd64/latest/amazon-cloudwatch-agent.deb
+if [$? -ne 0]; then
+    handle_error "Failed to get amazon-cloudwatch-agent.deb"
+fi
